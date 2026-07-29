6f925abafe302b673c2fd8d72b86401275b37bb6
evanhe <157073638+evan-h3@users.noreply.github.com>
2026-07-29 13:19:59 -0400

    Add: reorganize-string - submission-1

---
+++ Data Structures & Algorithms/reorganize-string/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        freq = Counter(s)
+        if max(freq.values()) > (len(s)+1)//2:
+            return ""
+        
+        res = [""] * len(s)
+        sorted_freq = sorted(list(freq.items()), key = lambda x:x[1], reverse="True")
+        index = 0
+        curr = 0
+
+        for l, f in sorted_freq:
+            for _ in range(f):
+                res[index] = l
+                index+=2
+                if index > len(s)-1:
+                    index = 1
+                
+        return "".join(res)
